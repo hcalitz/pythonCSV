e5487a85e50a24094c34a2a61f50e6953b65e8c0
Hannes Calitz <j.calitz@massey.ac.nz>
2016-07-05 12:58:45 +1200

    A generic CSV viewer

---
+++ doCSVplots.py
@@ -0,0 +1,37 @@
+# doCSVplots.py
+# Use program like this on the command line: 
+# python doplots.py filename x y [startrow] [stoprow]
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# For reading from the command line 
+import sys
+
+filename = sys.argv[1]
+
+# Read column headings from commandline
+xvar = sys.argv[2]
+yvar = sys.argv[3]
+
+if 'Date' in xvar or 'date' in xvar:
+    df = pd.read_csv(filename, header=0, usecols=[xvar,yvar], parse_dates=[xvar])
+    x= [pd.Timestamp.date(df[xvar][elem]) for elem in range(len(df[xvar]))]
+    usedates = 1
+else:
+    df = pd.read_csv(filename, header=0, usecols=[xvar,yvar])
+    usedates = 0
+
+# Optional: Read the range values from the command line 
+if len(sys.argv) > 4:
+    startrow = int(sys.argv[4])
+    stoprow = int(sys.argv[5])
+else:
+    startrow = 1
+    stoprow = len(df[xvar])
+if usedates:
+    plt.plot(x[startrow:stoprow],df[yvar][startrow:stoprow], label=yvar)
+    plt.xticks(rotation=30)
+else:
+	plt.plot(df[xvar][startrow:stoprow],df[yvar][startrow:stoprow], label=yvar)
+plt.show()

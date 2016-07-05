0dc966a80096eec5300fa294f9b5c1752af562bb
Hannes Calitz <j.calitz@massey.ac.nz>
2016-07-05 14:21:45 +1200

    Added program use examples

--- doCSVplots.py
+++ doCSVplots.py
@@ -1,10 +1,15 @@
 # doCSVplots.py
 # JJ Calitz 
 # July 2016
+# IFS Massey University
 # Palmerston North, New Zealand
 
 # Use program like this on the command line: 
-# python doplots.py filename x y [startrow] [stoprow]
+#     python doplots.py filename x y [startrow] [stoprow]
+# e.g.
+#     python doplots.py mydata.csv "Date" "Median Value" [startrow] [stoprow]
+# or if only certain lines are from eg 83 to 2000 are viewed
+#     python doplots.py mydata.csv "Date" "Median Value" 83 2000
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,7 +19,8 @@ import sys
 
 filename = sys.argv[1]
 
-# Read column headings from commandline
+# Read case sensitive column headings from commandline, 
+# enclose spaces in inverted commas, e.g. "1 st value" 
 xvar = sys.argv[2]
 yvar = sys.argv[3]
 
